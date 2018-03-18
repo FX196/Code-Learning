ab4ae6fef93cb0c7d43731deba4425331f0b6bfe
FX196 <mikechen212@foxmail.com>
2018-03-18 21:48:41 +0800

    rename project to code learning

---
+++ HackerRank/30 Days of Code/Extra-Long-Factorials.py
@@ -0,0 +1,10 @@
+def ExtraLongFactorials(n):
+    if n > 1:
+        return n * ExtraLongFactorials(n - 1)
+    else:
+        return 1
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    extraLongFactorials(n)
