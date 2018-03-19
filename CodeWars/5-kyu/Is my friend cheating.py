5c9ed8cc7e2e0c2349b2ae0bef6b79b9bc67a198
FX196 <mikechen212@foxmail.com>
2018-03-19 14:42:13 +0800

    code cleanup

--- CodeWars/5-kyu/Is my friend cheating.py
+++ CodeWars/5-kyu/Is my friend cheating.py
@@ -1,10 +1,10 @@
 def removNb(n):
     # your code
-    su = n*(n+1)/2
+    su = n * (n + 1) / 2
     res = []
-    for j in range(1, n+1):
-        s = su-j
-        i=j+1
-        if s/i == int(s/i) and s/i <= n:
-            res.append((j,int(s/i)))
+    for j in range(1, n + 1):
+        s = su - j
+        i = j + 1
+        if s / i == int(s / i) and s / i <= n:
+            res.append((j, int(s / i)))
     return res
