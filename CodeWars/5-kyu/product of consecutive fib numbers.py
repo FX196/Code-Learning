f9762971c7339a65bf4c7f21042bb61698250df3
FX196 <mikechen212@foxmail.com>
2018-03-19 14:57:18 +0800

    reformat

--- CodeWars/5-kyu/product of consecutive fib numbers.py
+++ CodeWars/5-kyu/product of consecutive fib numbers.py
@@ -1,6 +1,6 @@
 def productFib(prod):
     # your code
     l, m, n = 0, 0, 1
-    while m*n < prod:
-        l, m, n = m, n, m+n
-    return [m,n,m*n==prod]
+    while m * n < prod:
+        l, m, n = m, n, m + n
+    return [m, n, m * n == prod]
