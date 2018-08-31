f96498d7e00bb3ca1227d69a6a140b871ec5c203
yuhongc <mikechen212@foxmail.com>
2018-08-30 19:27:27 -0700

    Finish longest_substring

--- LeetCode/LengthOfLongestSubstring.py
+++ LeetCode/LengthOfLongestSubstring.py
@@ -4,8 +4,20 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        # TODO
+        if len(s) == 1:
+            return 1
+        longest = 0
+        i, j = 0, 0  # ptr 1 and 2
+        while j < len(s):  # keep j in range
+            if s[j] in s[i:j]:
+                longest = max(longest, j - i)
+                while s[j] in s[i:j]:
+                    i += 1
+            j += 1
+        longest = max(longest, j - i)
+        return longest
+
 
 
 if __name__ == "__main__":
-    print(Solution.lengthOfLongestSubstring(Solution, "bdb"))
+    print(Solution.lengthOfLongestSubstring(Solution, "dvdf"))
