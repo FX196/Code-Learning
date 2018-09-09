baeedacb95d8f1accc17c08e2f15b86b4397563b
yuhongc <mikechen212@foxmail.com>
2018-09-09 11:57:33 -0700

    Start #10 Leetcode

---
+++ LeetCode/10-Regular Expression Matching.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        while s and p:
+
+        else:
+            if not s and not p:
+                return True
+            else:
+                return False
