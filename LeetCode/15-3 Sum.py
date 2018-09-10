48f67c57d5ceaf47a7ca7c7843be122549569319
yuhongc <mikechen212@foxmail.com>
2018-09-10 15:44:53 -0700

    Finished #15 Leetcode; Ongoing: #10 Leetcode

---
+++ LeetCode/15-3 Sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums = sorted(nums)
+        for i in range(len(nums) - 2):
+            if i == 0 or nums[i] > nums[i - 1]:
+                start = i + 1
+                end = len(nums) - 1
+
+                while start < end:
+                    if nums[i] + nums[start] + nums[end] == 0:
+                        res.append([nums[i], nums[start], nums[end]])
+                    if nums[i] + nums[start] + nums[end] < 0:
+                        curStart = start
+                        while start < end and nums[start] == nums[curStart]:
+                            start += 1
+                    else:
+                        curEnd = end
+                        while start < end and nums[end] == nums[curEnd]:
+                            end -= 1
+        return res
+
+
+if __name__ == "__main__":
+    result = Solution.threeSum(Solution, [0, 0, 0])
+    print(result)
