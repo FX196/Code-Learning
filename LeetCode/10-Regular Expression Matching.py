eb4ff34c28035b9dcdb1295418c61e8ddf9622e4
yuhongc <mikechen212@foxmail.com>
2018-10-25 00:30:34 -0700

    Finished #10 Hard Leetcode Ongoing: #30,300 Leetcode

--- LeetCode/10-Regular Expression Matching.py
+++ LeetCode/10-Regular Expression Matching.py
@@ -1,27 +1,28 @@
-# TODO: unfinished
 class Solution:
     def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
-        last = ""
-        while s and p:
-            if p[-1] == s[-1]:
-                s, p = s[:-1], p[:-1]
-            elif p[-1] == "*":
-                while s[-1] == p[-1]:
-                    pass
-        else:
-            if not s and not p:
-                return True
-            else:
-                return False
+        mem = {}  # caches results for isMatch(s[i:], p[j:])
+
+        def dp(i, j):
+            if (i, j) not in mem:
+                if j == len(p):  # reached the end of p
+                    res = i == len(s)
+                else:
+                    first_match = i < len(s) and p[j] in {s[i], "."}  # the first characters of s and p match
+                    if j + 1 < len(p) and p[j + 1] == "*":
+                        # second character in p is *, so either it's
+                        # 0 times the first character or many times the first character
+                        res = dp(i, j + 2) or first_match and dp(i + 1, j)
+                        # can work either way, so it's or between the two conditions
+                    else:
+                        res = first_match and dp(i + 1, j + 1)
+                mem[i, j] = res
+            return mem[i, j]
+
+        return dp(0, 0)
 
 
 if __name__ == "__main__":
     s = input()
     p = input()
-    res = Solution.isMatch(Solution, s, p)
-    print(res)
+    result = Solution().isMatch(s, p)
+    print(result)
