ddb66d3dbdc8216373a785c47a581d2cb51209a0
yuhongc <mikechen212@foxmail.com>
2019-05-13 00:38:03 -0700

    201

---
+++ Python/LeetCode/201-Bitwise AND of Numbers Range.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 0 or 2*m <= n: return 0
+        for i in range(m+1, n+1):
+            m&=i
+        return m
